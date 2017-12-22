41682734d1f3587eebdf9fc26967dcfebaf6f9a0
dicRichie <dicRichie@users.noreply.github.com>
2017-12-22 16:00:39 -0500

    Add files via upload

---
+++ DB/MusicContext.cs
@@ -0,0 +1,27 @@
+﻿using AngularMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace AngularMVC.DB
+{
+    public class MusicContext : DbContext
+    {
+        public MusicContext() : base("Music")
+        {
+
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
