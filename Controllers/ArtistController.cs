2543ce6fe3195194c7eedad1de9b5b81560208e7
Richard Bakos <22459180+B-Richie@users.noreply.github.com>
2017-12-22 16:30:38 -0500

    Add files via upload

---
+++ Controllers/ArtistController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularMVC.DB;
+using Newtonsoft.Json;
+using AngularMVC.Models;
+
+namespace AngularMVC.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private MusicContext context = new MusicContext();
+        // GET: Artist
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ContentResult GetArtists()
+        {
+            var artists = context.Artists.Include("Albums.Tracks").ToList();
+            var list = JsonConvert.SerializeObject(artists, Formatting.None, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+            return Content(list, "application/json");
+        }
+
+        public string InsertArtist(Artist artist)
+        {
+            if(artist != null)
+            {
+                using (context)
+                {
+                    context.Artists.Add(artist);
+                    context.SaveChanges();
+                    return "Artist successfully added!";
+                }
+            }
+            else
+            {
+                return "Error inserting artist. Please try again.";
+            }
+        }
+    }
+}
