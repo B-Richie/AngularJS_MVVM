2c12d766be7d5c7a66aeb07cf44510564ef8a0b6
dicRichie <dicRichie@users.noreply.github.com>
2017-12-22 16:03:01 -0500

    Add files via upload

---
+++ ArtistClient.js
@@ -0,0 +1,49 @@
+﻿var ArtistApp = angular.module('ArtistApp', [])
+
+ArtistApp.controller('ArtistController', function ($scope, ArtistService) {
+
+    getArtists();
+
+
+    function getArtists() {
+        ArtistService.getArtists()
+        .then(function (response) {
+            //debugger
+            $scope.sortBy = 'ArtistName';
+            $scope.reverse = false;
+            $scope.artists = response.data;
+            console.log($scope.artists);
+
+        }, function () {
+            console.log($scope.artists);
+            alert("Error occurred");
+        });
+    }
+
+    function insertArtist() {
+        ArtistService.insertArtist()
+        .then(function (artistData) {
+            var artist = {
+                ArtistName: $scope.ArtistName
+            };
+        }, function () {
+            alert("Error inserting artist");
+        });
+    }
+});
+
+
+ArtistApp.factory('ArtistService', ['$http', function ($http) {
+
+    var ArtistService = {};
+    ArtistService.getArtists = function () {
+        return $http.get('/Artist/GetArtists');
+    };
+
+    ArtistService.insertArtist = function () {
+        $http.post('/Artist/InsertArtist', artistData);
+    }
+    return ArtistService;
+}]);
+
+
