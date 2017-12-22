2c7c2cb7ae6c1008660c704e06784d97006648ed
dicRichie <dicRichie@users.noreply.github.com>
2017-12-22 16:01:42 -0500

    Add files via upload

---
+++ Models/Artist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace AngularMVC.Models
+{
+    public class Artist
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ArtistID { get; set; }
+
+        [Required]
+        public string ArtistName { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+        //public IPagedList<Album> Albums { get; set; }
+    }
+}
